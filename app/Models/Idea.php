ee5e8a6eb5fdb1a7d6158b37651cb9765a72f893
nandofsl <nandoferfsl@gmail.com>
2026-02-20 16:46:10 -0300

    estilizando e autenticando

---
+++ app/Models/Idea.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use App\IdeaStatus;
+use App\Models\User;
+use App\Models\Step;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
+
+class Idea extends Model
+{
+    /** @use HasFactory<\Database\Factories\IdeaFactory> */
+    use HasFactory;
+
+    protected $casts = [
+        'links' => AsArrayObject::class,
+        'status' => IdeaStatus::class,
+    ];
+
+    protected $attributes = [
+        'status' => IdeaStatus::PENDING->value,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function steps(): HasMany
+    {
+        return $this->hasMany(Step::class);
+    }
+}
---
+++ resources/views/components/layout/nav.blade.php
@@ -0,0 +1,21 @@
+<nav class="border-b border-border px-6">
+  <div class="max-w-7xl mx-auto h-16 flex items-center justify-between">
+    <div>
+      <a href="/">
+        <img src="/images/logo.png" alt="Idea logo" width="100">
+      </a>
+    </div>
+    <div class="flex gap-x-5 items-center">
+      @auth
+      <form action="/logout" method="post">
+        @csrf
+        <button>Log Out</button>
+      </form>
+      @endauth
+      @guest
+      <a href="/login">Sign In</a>
+      <a href="/register" class="btn">Register</a>
+      @endguest
+    </div>
+  </div>
+</nav>
